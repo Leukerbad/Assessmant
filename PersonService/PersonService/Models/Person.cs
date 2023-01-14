cf84444047eb052129aadeb2e5c03eb3f37fc0c7
unknown <you@example.com>
2023-01-14 10:21:03 +0100

    File Zugriff mit CRUD Operations

---
+++ PersonService/PersonService/Models/Person.cs
@@ -0,0 +1,32 @@
+﻿namespace PersonService.Models
+{
+    public class Person
+    {
+        public string Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Gender { get; set; }
+        public bool Active { get; set; }
+
+        public string UserName { get; set; }
+        public string Notes { get; set; }
+
+        public Person(string id, string firstName, string lastName, string email, string gender, string active, string userName, string notes)
+        {
+            Id = id;
+            FirstName = firstName;
+            LastName = lastName;
+            Email = email;
+            Gender = gender;
+            Active = bool.Parse(active);
+            UserName = userName;
+            Notes = notes;
+        }
+
+        public override string ToString()
+        {
+            return $"\n{Id};{FirstName};{LastName};{Email};{Gender};{Active};{UserName};{Notes}";
+        }
+    }
+}
---
+++ PersonService/PersonService/Models/PersonContext.cs
@@ -0,0 +1,67 @@
+﻿namespace PersonService.Models
+{
+    public class PersonContext
+    {
+        public List<Person> People { get; set; }
+        private readonly string path = Path.Combine(Environment.CurrentDirectory, @"Database\","people.txt");
+        private string header;
+
+        public PersonContext()
+        {
+            People = LoadPeople();
+            header = GetHeader();
+            //CreatePerson(new Person("test", "test", "test", "test", "test", "True", "test", "test"));
+            //DeletePerson(GetPersonById("test"));
+            //UpdatePerson("test", new Person("test123", "test123", "tes123t", "tes123t", "test123", "True", "test", "test"));
+        }
+
+        private List<Person> LoadPeople()
+        {
+            People = File.ReadAllLines(path).Skip(1)
+                .Select(line => line.Split(';'))
+                .Select(attr => new Person(attr[0], attr[1], attr[2], attr[3], attr[4], attr[5], attr[6], attr[7]))
+                .ToList();
+
+            return People;
+        }
+
+        private string GetHeader()
+        {
+            return File.ReadLines(path).First();
+        }
+
+        private void SaveChanges()
+        {
+            File.WriteAllText(path, String.Empty);
+            File.WriteAllText(path, header);
+            foreach (var person in People)
+            {
+                File.AppendAllText(path, person.ToString());
+            }
+        }
+
+        public Person GetPersonById(string id)
+        {
+            return People.Single(p => p.Id == id);
+        }
+
+        public void CreatePerson(Person person)
+        {
+            People.Add(person);
+            SaveChanges();
+        }
+
+        public void DeletePerson(Person person)
+        {
+            People.Remove(person);
+            SaveChanges();
+        }
+
+        public void UpdatePerson(string id, Person person)
+        {
+            People.Remove(People.Single(p => p.Id == id));
+            People.Add(person);
+            SaveChanges();
+        }
+    }
+}
